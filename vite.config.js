882aef8550b5ed675104255dec68367ba0382eaa
lucomon <lucomon1907@gmail.com>
2025-09-18 22:38:57 +0300

    Simplify Vercel config

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
   }
 })
